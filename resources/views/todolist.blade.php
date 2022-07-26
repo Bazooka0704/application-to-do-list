2f8b30bf130837b11ee48509574c6caaafbe5a69
Bazooka0704 <yannask16@gmail.com>
2022-07-26 17:28:04 +0000

    Goood

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -140,6 +140,10 @@
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
         <script>
+            $(".btn-close").on('click', function(){
+                $("#ModalTask").modal('toggle');
+            })
+
             $("body").on('click','.editRow', function(event){
                 var id = $(this).data("id");
                 var Desc = $("#Name"+id).text();
