fdf6a3c36ff1f9f78d39c777ecc55bdaac6577ea
Bazooka0704 <yannask16@gmail.com>
2022-07-26 14:47:35 +0000

    message

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,10 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'tasks',
+        'updatetasks',
+        'deletetasks',
+        'trueendtask',
+        'falseendtask'
     ];
 }
