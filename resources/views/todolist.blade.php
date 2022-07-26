5978ca3b72353595eefdc0f48b06f8187a41aa98
Bazooka0704 <yannask16@gmail.com>
2022-07-26 20:59:01 +0000

    Goood

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -64,6 +64,7 @@
             </form>
         </div>
                 @if(count($tasks) > 0)
+                <div style="margin:auto; max-width:600px; width:80%">
                     <table class="table align-middle mb-0 bg-white" style="margin:auto; max-width:600px; width:80%">
                         <thead class="bg-light" style=" font-weight:bold; color: orange">
                             <th>Tâche</th>
@@ -110,8 +111,10 @@
                         @endforeach
                         </tbody>
                     </table>
+                    {!! $tasks->links() !!}
+                </div>
                 @endif
-        {!! $tasks->links() !!}
+
         @stack('scripts')
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" type="text/javascript"></script>
         <link rel="stylesheet" src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
