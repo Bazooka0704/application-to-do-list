8bfa6b09da091876e22c1418b1b66aad33ac26ed
Bazooka0704 <yannask16@gmail.com>
2022-07-26 17:45:46 +0000

    Goood

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -64,10 +64,10 @@
             </form>
             <br>
             <br>
-            <div class="container">
+            <div class="container" style="width: 100%">
                 <div class="row">
                     @if(count($tasks) > 0)
-                        <table class="table align-middle mb-0 bg-white">
+                        <table class="table align-middle mb-0 bg-white" style="width: 100%">
                             <thead class="bg-light" style=" font-weight:bold; color: orange">
                                 <th>Tâche</th>
                                 <th>Description</th>
