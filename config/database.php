855d9dd15accbe0b2c82fc0bf9d1cfc16eba2d58
Bazooka0704 <yannask16@gmail.com>
2022-07-26 14:12:23 +0000

    message

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,17 +65,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL','postgres://rctybapnrszgla:d05329160da3bd524b5aaadf9b31235fc708b31fd7c125f08504ecb26ae7f2de@ec2-52-49-120-150.eu-west-1.compute.amazonaws.com:5432/d9u8ocekk3hsrd'),
+            'host' => env('DB_HOST', 'ec2-52-49-120-150.eu-west-1.compute.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd9u8ocekk3hsrd'),
+            'username' => env('DB_USERNAME', 'rctybapnrszgla'),
+            'password' => env('DB_PASSWORD', 'd05329160da3bd524b5aaadf9b31235fc708b31fd7c125f08504ecb26ae7f2de'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
