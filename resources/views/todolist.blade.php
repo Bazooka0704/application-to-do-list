1bf1c910e016773175e9e4be5979716b81bf8c1a
Bazooka0704 <yannask16@gmail.com>
2022-07-26 14:38:18 +0000

    message

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -116,18 +116,28 @@
             {!! $tasks->links() !!}
         </div>
         @stack('scripts')
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <script src="{{ url('/jquery/jquery.min.js')}}" type="text/javascript"></script>
-        <link rel="stylesheet" href="{{ url('bootstrap/bootstrap.min.css')}}" type="text/css">
-        <link rel="stylesheet" href="{{ url('/jquery/jquery-ui.min.css')}}" type="text/css"/>
-        <script src="{{ url('/jquery/jquery-ui.min.js')}}" type="text/javascript"></script>
-        <link rel="stylesheet" href="{{ url('font-awesome/all.min.css')}}" type="text/css"/>
-        <link rel="stylesheet" href="{{ url('mdbootstrap/mdb.min.css')}}" type="text/css"/>
-        <script src="{{ url('mdbootstrap/mdb.min.js')}}" type="text/javascript"></script>
-        <script src="{{ url('momentjs/moment.min.js')}}" type="text/javascript"></script>
-        <script src="{{ url('sweetalertjs/sweetalert.min.js')}}" type="text/javascript"></script>
-        <script src="{{ url('popperjs/popper.min.js') }}" type="text/javascript"></script>
-        <script src="{{ url('bootstrap/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+        <link rel="stylesheet" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" type="text/css"/>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" type="text/javascript"></script>
+        <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        rel="stylesheet"
+        />
+        <!-- Google Fonts -->
+        <link
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        rel="stylesheet"
+        />
+        <!-- MDB -->
+        <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.3.0/mdb.min.css"
+        rel="stylesheet"
+        />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js" type="text/javascript"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" type="text/javascript"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
         <script>
             $("body").on('click','.editRow', function(event){
                 var id = $(this).data("id");
